94126c65c597921704450ca7a7100017807a190d
FabPaul <paulfopa0@gmail.com>
2024-02-06 10:51:38 +0100

    Added language, default locale  and default timezone.

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config(object):
+    """language class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULTLOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+@app.route('/')
+def index_0():
+    """function that returns Welcome to Holberton"""
+    return render_template('0-index.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
