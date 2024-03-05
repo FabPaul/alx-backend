786ebc87833885f584bae9ec3cebe31225ac4e06
FabPaul <paulfopa0@gmail.com>
2024-03-05 17:13:29 +0100

    Advanced task

---
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const redis = require('redis');
+const kue = require('kue');
+const { promisify } = require('util');
+
+const app = express();
+const port = 1245;
+
+const redisClient = redis.createClient();
+
+const getAsync = promisify(redisClient.get).bind(redisClient);
+const setAsync = promisify(redisClient.set).bind(redisClient);
+
+const queue = kue.createQueue();
+
+async function reserveSeat(number) {
+  await setAsync('available_seats', number);
+}
+
+async function getCurrentAvailableSeats() {
+  const seats = await getAsync('available_seats');
+  return parseInt(seats) || 0;
+}
+
+reserveSeat(50);
+
+let reservationEnabled = true;
+
+app.get('/available_seats', async (req, res) => {
+  const numberOfAvailableSeats = await getCurrentAvailableSeats();
+  res.json({ numberOfAvailableSeats });
+});
+
+app.get('/reserve_seat', async (req, res) => {
+  if (!reservationEnabled) {
+    res.json({ status: 'Reservation are blocked' });
+    return;
+  }
+
+  queue.create('reserve_seat').save((err) => {
+    if (err) {
+      res.json({ status: 'Reservation failed' });
+    } else {
+      res.json({ status: 'Reservation in process' });
+    }
+  });
+});
+
+app.get('/process', async (req, res) => {
+  res.json({ status: 'Queue processing' });
+
+  queue.process('reserve_seat', async (job, done) => {
+    const availableSeats = await getCurrentAvailableSeats();
+    if (availableSeats <= 0) {
+      reservationEnabled = false;
+      done(new Error('Not enough seats available'));
+    } else {
+      await reserveSeat(availableSeats - 1);
+      if (availableSeats - 1 === 0) {
+        reservationEnabled = false;
+      }
+      done();
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
